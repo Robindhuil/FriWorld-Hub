a7c7846f6a6193e544e2c20cb410e6fb17bd1193
Robindhuil <robogasso@gmail.com>
2026-09-04 19:51:13 +0200

    feat(frilens): odkaz na 0.1.8-alpha
    Doladenie tlačidiel, logu a dokumentácie pred 0.2.0.

--- src/content/frilens.ts
+++ src/content/frilens.ts
@@ -7,9 +7,9 @@
 // inak release assety vrátia každému okrem majiteľa chybu 404.
 
 export const frilens = {
-  version: '0.1.7-alpha',
+  version: '0.1.8-alpha',
 
-  apk: 'https://github.com/Robindhuil/FriLens/releases/download/v0.1.7-alpha/FriLens-0.1.7-alpha.apk',
+  apk: 'https://github.com/Robindhuil/FriLens/releases/download/v0.1.8-alpha/FriLens-0.1.8-alpha.apk',
   apkMb: 40,
 
   minAndroid: '7.1',
