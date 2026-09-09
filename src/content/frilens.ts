5cfab3263dad033c1d43a3c16294458737f54962
Robindhuil <robogasso@gmail.com>
2026-09-09 13:22:35 +0200

    feat(frilens): odkaz na 0.2.1-alpha

--- src/content/frilens.ts
+++ src/content/frilens.ts
@@ -7,9 +7,9 @@
 // inak release assety vrátia každému okrem majiteľa chybu 404.
 
 export const frilens = {
-  version: '0.2.0-alpha',
+  version: '0.2.1-alpha',
 
-  apk: 'https://github.com/Robindhuil/FriLens/releases/download/v0.2.0-alpha/FriLens-0.2.0-alpha.apk',
+  apk: 'https://github.com/Robindhuil/FriLens/releases/download/v0.2.1-alpha/FriLens-0.2.1-alpha.apk',
   apkMb: 40,
 
   minAndroid: '7.1',
